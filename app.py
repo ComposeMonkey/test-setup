a5340f16eacc3f8df6103746518b0baf597de8af
Paul <ppymou@gmail.com>
2015-06-21 03:11:36 +0200

    first commit

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask
+from redis import Redis
+
+import os
+
+app = Flask(__name__)
+redis = Redis(host="redis_1", port=6379)
+
+@app.route("/")
+def hello():
+  redis.incr("hits")
+  return 'HW'
+
+if __name__ == "__main__":
+  app.run(host="0.0.0.0", debug=True)
